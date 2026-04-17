5fe5d8322be39359b56208473682040efc90811f
Giventarou <givenassersianturi@gmail.com>
2026-04-17 10:02:33 +0700

    first push

---
+++ src/main/java/com/example/productcrud/ProductCrudApplication.java
@@ -0,0 +1,14 @@
+package com.example.productcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCrudApplication {
+//Menambahkan fitur A
+//Menambahkan fitur B
+//Menambahkan fitur C
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCrudApplication.class, args);
+    }
+}
